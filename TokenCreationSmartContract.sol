a1a529d04769967b45eeee1c3b5a2b741c7a3788
altcoinrockerxyz <sam@oceanmoon.rocks>
2017-11-04 10:32:13 +0800

    Minor Updates to Initial SC code

--- TokenCreationSmartContract.sol
+++ TokenCreationSmartContract.sol
@@ -1,3 +1,5 @@
+    /// @notice Initial code copied from https://www.ethereum.org/token
+
 pragma solidity ^0.4.16;
 
 interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public; }
